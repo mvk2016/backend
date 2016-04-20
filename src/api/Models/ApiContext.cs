645d5c4cac2d2b9a82a9b37898f1b75e360ee446
Mauritz <mauritzz@kth.se>
2016-04-20 02:12:30 +0200

    Made all data entities directly queryable

--- src/api/Models/ApiContext.cs
+++ src/api/Models/ApiContext.cs
@@ -9,5 +9,8 @@ namespace api.Models
     public class ApiContext : DbContext
     {
         public DbSet<Building> Buildings { get; set; }
+        public DbSet<Floor> Floors { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<SensorData> SensorData { get; set; }
     }
 }
